ddeaf5c076a6b2bb7e1649df26c6c154b2d8ce3a
MaxatDzhetenbayev <yaboku18@mail.ru>
2023-03-05 15:35:20 +0600

    deploy

--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import { rootReduser } from "./rootReduser";
-import { devToolsEnhancer } from 'redux-devtools-extension'
-
-
-export const store = createStore(rootReduser, devToolsEnhancer())
--- src/store/rootReduser.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { positionsReduser } from './positions/positions-reduser'
-import { filterReduser } from './filter/filter-reduser'
-
-export const rootReduser = combineReducers({
-   positions: positionsReduser,
-   filters: filterReduser
-})
